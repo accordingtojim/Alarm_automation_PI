feb106799be41a5e61c65eb944bd428d18bb757f
Jimmy Carradore <carradorejimmy@gmail.com>
2022-12-04 12:05:43 +0800

    debugging

--- main.py
+++ main.py
@@ -95,9 +95,10 @@ config.n_PI = number_of_PI.get()
 if config.n_PI != '' and config.n_PI != 0:
 # energy house
     gen_EH05.gen_BB_EH05.file_creation_1('./template_excel/template_BB_samsung.xlsx')
+    gen_EH05.gen_BR_EH05.file_creation_6('./template_excel/template_BR_samsung.xlsx')
     gen_EH05.gen_PEMS_EH05.file_creation_3('./template_excel/template_PEMS_EH05.xlsx')
     gen_EH05.gen_HVAC_EH05.file_creation_4('./template_excel/template_HVAC_EH05.xlsx')
-    gen_EH05.gen_BR_EH05.file_creation_6('./template_excel/template_BR_samsung.xlsx')
+    
     # power house
 if  len(config.array_PH) != 0:
     gen_PH2.gen_INV_PH2.file_creation_0('./template_excel/template_CL_CBESSHD.xlsx')
