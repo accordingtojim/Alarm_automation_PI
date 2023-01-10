3e2e5bca0de599ff6495d3ab51459598406cce19
Jimmy Carradore <carradorejimmy@gmail.com>
2023-01-10 08:14:53 +0100

    versione finale funzionante: rimossa la ibrida e acquisizione dati nel main da file excel_sviluppo

--- main.py
+++ main.py
@@ -1,120 +1,77 @@
-from tkinter import *
-from tkinter.ttk import *
-from tkinter.messagebox import showinfo
-import array
-import config
 from openpyxl import load_workbook
 from openpyxl import Workbook
-import shutil
 import os
-import define_hybrid_house as dhh
-import new_window_1 as nw
+import array
+import config
 import gen_EH05.gen_BB_EH05,gen_EH05.gen_BR_EH05,gen_EH05.gen_HVAC_EH05,gen_EH05.gen_PEMS_EH05
 import gen_PH2.gen_AUX_PH2,gen_PH2.gen_INV_PH2,gen_PH2.gen_PEMS_PH2,gen_PH2.gen_SKID_PH2
-import gen_HH.gen_AUX_HH,gen_HH.gen_BB_HH, gen_HH.gen_BR_HH,gen_HH.gen_HVAC_HH,gen_HH.gen_INV_HH,gen_HH.gen_PEMS_HH,gen_HH.gen_SKID_HH
-# root window
-root = Tk()
-root.geometry("700x300")
-root.resizable(True, True)
-root.title('Number of Power Island')
 
-number_of_PI = StringVar()
-number_of_HH = StringVar()
+wb = load_workbook('excel_sviluppo.xlsx')    
+ws = wb.active
 
-def PI_clicked():
-    # callback when the login button clicked
-    msg = f'You entered email: {number_of_PI.get()}' 
-    showinfo(title='Information',message=msg)
+config.n_PI = ws['L2'].value
 
-# empty label 1
-empty_label = Label(root, text="")
-empty_label.grid(row=0,column=0,columnspan=3,sticky=N,pady=20)
+#array acquisition HVAC
+config.n_HVAC.append(ws['B3'].value)
+config.n_HVAC.append(ws['B16'].value)
+config.n_HVAC.append(ws['B29'].value)
+config.n_HVAC.append(ws['B42'].value)
 
-# PI label 
-PI_label = Label(root, text="Insert the number of total Power Island")
-PI_label.grid(row=1,column=2,padx=150)
+#battery bank acquisition
+config.n_battery_bank.append(ws['B4'].value)
+config.n_battery_bank.append(ws['B17'].value)
+config.n_battery_bank.append(ws['B30'].value)
+config.n_battery_bank.append(ws['B43'].value)
 
-# empty label 2
-empty_label = Label(root, text="")
-empty_label.grid(row=2,column=0,columnspan=3,sticky=N,pady=0)
+#battery rack acquisition
+config.n_battery_rack.append(ws['B5'].value)
+config.n_battery_rack.append(ws['B18'].value)
+config.n_battery_rack.append(ws['B31'].value)
+config.n_battery_rack.append(ws['B44'].value)
 
-# PI entry
-PI_entry = Entry(root, textvariable=number_of_PI)
-PI_entry.grid(row=3,column=2)
-PI_entry.focus()
+#n CBESS acquisition
+config.n_CBESS.append(ws['B6'].value)
+config.n_CBESS.append(ws['B19'].value)
+config.n_CBESS.append(ws['B32'].value)
+config.n_CBESS.append(ws['B45'].value)
 
-# empty label 3
-empty_label = Label(root, text="")
-empty_label.grid(row=4,column=0,columnspan=3,sticky=N,pady=20)
+#n other inverter acquisition
+config.n_other_inverter.append(ws['B7'].value)
+config.n_other_inverter.append(ws['B20'].value)
+config.n_other_inverter.append(ws['B33'].value)
+config.n_other_inverter.append(ws['B46'].value)
 
-# HH label 
-HH_label = Label(root, text="Insert the number of Hybrid House")
-HH_label.grid(row=5,column=2,padx=150)
+#array type acquisition
+config.array_type.append(ws['B2'].value)
+config.array_type.append(ws['B15'].value)
+config.array_type.append(ws['B28'].value)
+config.array_type.append(ws['B41'].value)
 
-# empty label 4
-empty_label = Label(root, text="")
-empty_label.grid(row=6,column=0,columnspan=3,sticky=N,pady=0)
+#n EH acquisition
+config.array_EH.append(ws['B8'].value)
+config.array_EH.append(ws['B21'].value)
+config.array_EH.append(ws['B34'].value)
+config.array_EH.append(ws['B47'].value)
 
-# HH entry
-HH_entry = Entry(root, textvariable=number_of_HH)
-HH_entry.grid(row=7,column=2)
+#n PH acquisition
+config.array_PH.append(ws['B9'].value)
+config.array_PH.append(ws['B22'].value)
+config.array_PH.append(ws['B35'].value)
+config.array_PH.append(ws['B48'].value)
 
-# empty label 5
-empty_label = Label(root, text="")
-empty_label.grid(row=8,column=0,columnspan=3,sticky=N,pady=20)
+wb.close()
 
-# Define HH button
-OK_button = Button(root,text='Define HH', command=dhh.openNewWindow)
-OK_button.grid(row=7,column=2,padx=10,sticky=E)
-
-# next button
-next_button = Button(root,text='Define PI', command=nw.openNewWindow)
-next_button.grid(row=3,column=2,padx=10,sticky=E)
-
-# empty label
-# empty_label = Label(root, text="")
-# empty_label.grid(row=10,column=0,columnspan=3,sticky=N,pady=0)
-
-# exit button
-exit_button = Button(root,text='Exit',command=lambda:config.destroy_ww(root))
-exit_button.grid(row=7,column=1,padx=20)
-
-# quit button
-# exit_button = Button(root,text='Quit',command=root.quit)
-# exit_button.grid(row=11,column=3,padx=20)
-
-root.mainloop()
-
-#FINE PARTE GRAFICA
-###############################################################################
-#INIZIO GESTIONE FILE EXCEL
-
-config.num_HH_GUI = number_of_HH.get()
-config.n_PI = number_of_PI.get()
-
-if config.n_PI != '' and config.n_PI != 0:
 # energy house
-    gen_EH05.gen_BB_EH05.file_creation_1('./template_excel/template_BB_samsung.xlsx')
-    gen_EH05.gen_BR_EH05.file_creation_6('./template_excel/template_BR_samsung.xlsx')
-    gen_EH05.gen_PEMS_EH05.file_creation_3('./template_excel/template_PEMS_EH05.xlsx')
-    gen_EH05.gen_HVAC_EH05.file_creation_4('./template_excel/template_HVAC_EH05.xlsx')
+gen_EH05.gen_BB_EH05.file_creation_1('./template_excel/template_BB_samsung.xlsx')
+gen_EH05.gen_BR_EH05.file_creation_6('./template_excel/template_BR_samsung.xlsx')
+gen_EH05.gen_PEMS_EH05.file_creation_3('./template_excel/template_PEMS_EH05.xlsx')
+gen_EH05.gen_HVAC_EH05.file_creation_4('./template_excel/template_HVAC_EH05.xlsx')
     
-    # power house
-# if  len(config.array_PH) != 0:
-#     gen_PH2.gen_INV_PH2.file_creation_0('./template_excel/template_CL_CBESSHD.xlsx')
-#     gen_PH2.gen_AUX_PH2.file_creation_2('./template_excel/template_AUX_CBESSHD.xlsx')
-#     gen_PH2.gen_PEMS_PH2.file_creation_3('./template_excel/template_PEMS_PH2HD.xlsx')
-#     gen_PH2.gen_SKID_PH2.file_creation_5('./template_excel/template_SKID_PH2HD.xlsx')
-# hybrid house
-if config.num_HH_GUI != '':
-    gen_HH.gen_INV_HH.file_creation_0('./template_excel/template_CL_CBESSHD.xlsx')
-    gen_HH.gen_BB_HH.file_creation_1('./template_excel/template_BB_samsung.xlsx')
-    gen_HH.gen_AUX_HH.file_creation_2('./template_excel/template_AUX_CBESSHD.xlsx')
-    gen_HH.gen_HVAC_HH.file_creation_4('./template_excel/template_HVAC_HH.xlsx')
-    gen_HH.gen_PEMS_HH.file_creation_3('./template_excel/template_PEMS_HH.xlsx')
-    gen_HH.gen_SKID_HH.file_creation_5('./template_excel/template_SKID_HH.xlsx')
-    gen_HH.gen_BR_HH.file_creation_6('./template_excel/template_BR_samsung.xlsx')
-
+# power house
+gen_PH2.gen_INV_PH2.file_creation_0('./template_excel/template_CL_PH2HD.xlsx')
+gen_PH2.gen_AUX_PH2.file_creation_2('./template_excel/template_AUX_PH2HD.xlsx')
+gen_PH2.gen_PEMS_PH2.file_creation_3('./template_excel/template_PEMS_PH2HD.xlsx')
+gen_PH2.gen_SKID_PH2.file_creation_5('./template_excel/template_SKID_PH2HD.xlsx')
 
 config.file_aggregation(config.global_list)
 config.file_removal(config.global_list)
@@ -123,10 +80,3 @@ config.file_header('new_list_alarm.xlsx')
 config.file_numbering('new_list_alarm.xlsx')
 os.remove('list_alarm.xlsx')
 print ('Program ended!')
-
-
-
-    
-    
-    
-
--- test.py
+++ test.py
@@ -1,1 +1,8 @@
-
+from openpyxl import load_workbook
+from openpyxl import Workbook
+import os
+import array
+import config
+array =[]
+for i in range (1,0):
+    print('ok')
